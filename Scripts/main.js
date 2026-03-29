5214e1489ddd205a7243e769cb7443588c7bf4ea
Caroline Poveda <caroline.poveda.fl@gmail.com>
2026-03-29 20:11:43 +0200

    Nouveau header

--- Scripts/main.js
+++ Scripts/main.js
@@ -2,6 +2,6 @@ const NAV = document.getElementById('nav');
 const BTN_MENU = document.getElementById('menu');
 
 BTN_MENU.addEventListener('click', () => {
-    NAV.classList.toggle('nav-hide');
+    // NAV.classList.toggle('nav-hide');
     NAV.classList.toggle('nav-show');
 });
